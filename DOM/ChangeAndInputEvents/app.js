968232a21cac67c2e63ae45600d84329254c4609
lukvad <35584815+lukvad@users.noreply.github.com>
2022-12-14 23:31:04 +0100

    Input change

--- DOM/ChangeAndInputEvents/app.js
+++ DOM/ChangeAndInputEvents/app.js
@@ -1,10 +1,5 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
-
-// input.addEventListener('change', function (e) {
-//     console.log("CASKDJASKJHD")
-// })
-
+const input = document.querySelector('input')
+const h1 = document.querySelector('h1')
 input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
+    h1.innerText = input.value
 })
--- DOM/TheEventObject/app.js
+++ DOM/TheEventObject/app.js
@@ -22,14 +22,51 @@
 // })
 
 
-const form = document.querySelector('#shelterForm')
-const input = document.querySelector('#catName ')
-const list = document.querySelector('#cats')
-form.addEventListener('submit', function (e) {
+// const form = document.querySelector('#shelterForm')
+// const input = document.querySelector('#catName ')
+// const list = document.querySelector('#cats')
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     const catName = input.value
+//     newLi = document.createElement('li')
+//     newLi.innerText = catName
+//     list.append(newLi)
+//     input.value = ''
+// })
+
+const tweetContainer = document.querySelector('#tweets')
+const tweetForm = document.querySelector('#tweetForm')
+tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    const catName = input.value
-    newLi = document.createElement('li')
-    newLi.innerText = catName
-    list.append(newLi)
-    input.value = ''
+    const username = tweetForm.elements.username
+    const tweet = this.elements.tweet
+    addTweet(username.value, tweet.value)
+    tweet.value = ''
+    username.value = ''
 })
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li')
+    newTweet.append(`${username} - ${tweet}`)
+    tweetContainer.append(newTweet)
+}
+
+
+const form = document.querySelector('form');
+const list = document.querySelector('#list')
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const product = this.elements.product
+    const qty = this.elements.qty
+    addProduct(product.value, qty.value)
+    qty.value = ''
+    product.value = ''
+})
+
+function addProduct(product, qty) {
+    const listElement = document.createElement('li')
+    listElement.append(`${product} - ${qty} pcs`)
+    list.append(listElement)
+}
