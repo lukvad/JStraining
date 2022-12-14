960b77fc64471d24e5a3be86075406973671dca4
lukvad <35584815+lukvad@users.noreply.github.com>
2022-12-14 12:39:27 +0100

    EVENTS

--- DOM/EventBasics/app.js
+++ DOM/EventBasics/app.js
@@ -1,39 +1,72 @@
-const btn = document.querySelector('#v2');
-
-btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    return `rgb(${r}, ${g}, ${b})`
 }
 
-function scream() {
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
-}
+const buttons = document.querySelector('button')
 
-btn.onmouseenter = scream;
-
-
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        button.style.backgroundColor = randomColor();
+    })
 }
 
 
-const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
-    alert("CLICKED!");
-})
 
-function twist() {
-    console.log("TWIST!")
-}
-function shout() {
-    console.log("SHOUT!")
-}
 
-const tasButton = document.querySelector('#tas');
+// const h1 = document.querySelector('h1')
+// button.addEventListener('click', () => {
+//     const newColor = randomColor()
+//     h1.innerText = newColor
+//     document.body.style.backgroundColor = newColor
+// })
 
-// tasButton.onclick = twist;
-// tasButton.onclick = shout;
 
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+
+// const btn = document.querySelector('#v2');
+
+// btn.onclick = function () {
+//     console.log("YOU CLICKED ME!")
+//     console.log("I HOPE IT WORKED!!")
+// }
+
+// function scream() {
+//     console.log("AAAAAHHHHH");
+//     console.log("STOP TOUCHING ME!")
+// }
+// function twist() {
+//     console.log("TWIST")
+// }
+
+// btn.onmouseenter = scream;
+
+
+// document.querySelector('h1').onclick = () => {
+//     alert('you clicked the h1!')
+// }
+
+
+// const btn3 = document.querySelector('#v3')
+// btn3.addEventListener('mouseup', function () {
+//     alert('clicked! ')
+// })
+
+// const btnTwist = document.querySelector('#tas')
+// btnTwist.addEventListener('click', twist)
+// btnTwist.addEventListener('click', scream, { once: true })
+
+
+
+
+// const btnH = document.querySelector('#hello')
+// const btnG = document.querySelector('#goodbye')
+
+// btnH.addEventListener('click', () => {
+//     console.log('hello')
+// })
+
+// btnH.addEventListener('click', () => {
+//     console.log('goodbye')
+// })
