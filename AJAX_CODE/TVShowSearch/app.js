a40a0a2d498f011581ce73e52808b205ad42fea0
lukvad <35584815+lukvad@users.noreply.github.com>
2023-01-04 00:11:20 +0100

    Ajax

--- AJAX_CODE/TVShowSearch/app.js
+++ AJAX_CODE/TVShowSearch/app.js
@@ -1,10 +1,24 @@
 
 const form = document.querySelector('#searchForm')
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', async function (e) {
     e.preventDefault()
-    console.log(form.elements.query);
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get('https://api.tvmaze.com/search/shows', config)
+    makeImages(res.data)
+    form.elements.query.value = ''
 })
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG')
+            img.src = result.show.image.medium
+            document.body.append(img)
+        }
+    }
+}
 
 // const form = document.querySelector('#searchForm');
 // form.addEventListener('submit', async function (e) {
